3c3e490d0d15d7a8f539ed06f9962ed9427f9e02
Surendar KS <103345824+surendar-05@users.noreply.github.com>
2024-10-20 04:42:26 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return "Hello, World!"
